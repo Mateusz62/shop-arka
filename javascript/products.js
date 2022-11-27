a05937fd68fba0e4c8fec4cfc713b2476c374c5d
twojeimie <twojemail@onet.pl>
2022-11-27 14:36:49 +0100

    Dodany wygląda countera do koszyka i dodane pliki javascriptowe

---
+++ javascript/products.js
@@ -0,0 +1,91 @@
+let products = [
+  {
+    id: 1,
+    name: "Sweter Luke czarny",
+    img: "./image/bestseller/golf.jpg",
+    price: 70,
+    type: "Swetry",
+    sizes: ["M", "L", "XL", "XXL"],
+  },
+
+  {
+    id: 2,
+    name: "Sweter okrągły dekolt Jack",
+    img: "./image/bestseller/pikowanakurtka.jpg",
+    price: 70,
+    type: "Swetry",
+    sizes: ["M", "L", "XL", "XXL"],
+  },
+
+  {
+    id: 3,
+    name: "Sweter okrągły dekolt Jack",
+    img: "./image/bestseller/skorzanakurtka.jpg",
+    price: 70,
+    type: "Swetry",
+    sizes: ["M", "L", "XL", "XXL"],
+  },
+
+  {
+    id: 4,
+    name: "Sweter okrągły dekolt Jack",
+    img: "./image/bestseller/pikowanakurtka.jpg",
+    price: 70,
+    type: "Swetry",
+    sizes: ["M", "L", "XL", "XXL"],
+  },
+
+  {
+    id: 5,
+    name: "Sweter okrągły dekolt Jack",
+    img: "./image/bestseller/skorzanakurtka.jpg",
+    price: 70,
+    type: "Swetry",
+    sizes: ["M", "L", "XL", "XXL"],
+  },
+
+  {
+    id: 6,
+    name: "Sweter okrągły dekolt Jack",
+    img: "./image/bestseller/pikowanakurtka.jpg",
+    price: 70,
+    type: "Swetry",
+    sizes: ["M", "L", "XL", "XXL"],
+  },
+
+  {
+    id: 7,
+    name: "Sweter okrągły dekolt Jack",
+    img: "./image/bestseller/skorzanakurtka.jpg",
+    price: 70,
+    type: "Swetry",
+    sizes: ["M", "L", "XL", "XXL"],
+  },
+
+  {
+    id: 8,
+    name: "Sweter okrągły dekolt Jack",
+    img: "./image/bestseller/pikowanakurtka.jpg",
+    price: 70,
+    type: "Swetry",
+    sizes: ["M", "L", "XL", "XXL"],
+  },
+
+  {
+    id: 9,
+    name: "Sweter okrągły dekolt Jack",
+    img: "./image/bestseller/skorzanakurtka.jpg",
+    price: 70,
+    type: "Swetry",
+    sizes: ["M", "L", "XL", "XXL"],
+  },
+
+  {
+    id: 10,
+    name: "Sweter okrągły dekolt Jack",
+    img: "./image/bestseller/pikowanakurtka.jpg",
+    price: 70,
+    type: "Swetry",
+    sizes: ["M", "L", "XL", "XXL"],
+  },
+];
